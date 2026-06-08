930fb8485043c8419cee3d2dde47fac4be29f839
Nurbu <lamanurbu2@gmail.com>
2026-06-08 13:41:58 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/sneakerdrops/SneakerDrops1Application.java
@@ -0,0 +1,13 @@
+package com.pluralsight.sneakerdrops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerDrops1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneakerDrops1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/pluralsight/sneakerdrops/SneakerDrops1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.sneakerdrops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerDrops1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
